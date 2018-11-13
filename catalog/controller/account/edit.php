27d45c7bf884cd2194dd3574f33d63c9a2a091a7
MikeChipshop <mike@miniman-webdesign.co.uk>
2018-11-13 12:10:22 +0000

    Added validation to custom fields

---
+++ catalog/controller/account/edit.php
@@ -0,0 +1,235 @@
+<?php
+class ControllerAccountEdit extends Controller {
+	private $error = array();
+
+	public function index() {
+		if (!$this->customer->isLogged()) {
+			$this->session->data['redirect'] = $this->url->link('account/edit', '', 'SSL');
+
+			$this->redirect($this->url->link('account/login', '', 'SSL'));
+		}
+
+		$this->language->load('account/edit');
+		
+		$this->document->setTitle($this->language->get('heading_title'));
+		
+		$this->load->model('account/customer');
+		
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+			$this->model_account_customer->editCustomer($this->request->post);
+			
+			$this->session->data['success'] = $this->language->get('text_success');
+
+			$this->redirect($this->url->link('account/account', '', 'SSL'));
+		}
+
+      	$this->data['breadcrumbs'] = array();
+
+      	$this->data['breadcrumbs'][] = array(
+        	'text'      => $this->language->get('text_home'),
+			'href'      => $this->url->link('common/home'),     	
+        	'separator' => false
+      	); 
+
+      	$this->data['breadcrumbs'][] = array(
+        	'text'      => $this->language->get('text_account'),
+			'href'      => $this->url->link('account/account', '', 'SSL'),        	
+        	'separator' => $this->language->get('text_separator')
+      	);
+
+      	$this->data['breadcrumbs'][] = array(
+        	'text'      => $this->language->get('text_edit'),
+			'href'      => $this->url->link('account/edit', '', 'SSL'),       	
+        	'separator' => $this->language->get('text_separator')
+      	);
+		
+		$this->data['heading_title'] = $this->language->get('heading_title');
+
+		$this->data['text_your_details'] = $this->language->get('text_your_details');
+
+		$this->data['entry_firstname'] = $this->language->get('entry_firstname');
+		$this->data['entry_lastname'] = $this->language->get('entry_lastname');
+		$this->data['entry_email'] = $this->language->get('entry_email');
+		$this->data['entry_telephone'] = $this->language->get('entry_telephone');
+		$this->data['entry_fax'] = $this->language->get('entry_fax');
+		$this->data['entry_years_trading'] = $this->language->get('entry_years_trading');
+		$this->data['entry_business_type'] = $this->language->get('entry_business_type');
+		$this->data['entry_how_did_you_hear'] = $this->language->get('entry_how_did_you_hear');
+
+		$this->data['button_continue'] = $this->language->get('button_continue');
+		$this->data['button_back'] = $this->language->get('button_back');
+
+		if (isset($this->error['warning'])) {
+			$this->data['error_warning'] = $this->error['warning'];
+		} else {
+			$this->data['error_warning'] = '';
+		}
+
+		if (isset($this->error['firstname'])) {
+			$this->data['error_firstname'] = $this->error['firstname'];
+		} else {
+			$this->data['error_firstname'] = '';
+		}
+
+		if (isset($this->error['lastname'])) {
+			$this->data['error_lastname'] = $this->error['lastname'];
+		} else {
+			$this->data['error_lastname'] = '';
+		}
+		
+		if (isset($this->error['email'])) {
+			$this->data['error_email'] = $this->error['email'];
+		} else {
+			$this->data['error_email'] = '';
+		}	
+		if (isset($this->error['years_trading'])) {
+			$this->data['error_years_trading'] = $this->error['years_trading'];
+		} else {
+			$this->data['error_years_trading'] = '';
+		}	
+		if (isset($this->error['business_type'])) {
+			$this->data['error_business_type'] = $this->error['business_type'];
+		} else {
+			$this->data['error_business_type'] = '';
+		}	
+		if (isset($this->error['how_did_you_hear'])) {
+			$this->data['error_how_did_you_hear'] = $this->error['how_did_you_hear'];
+		} else {
+			$this->data['error_how_did_you_hear'] = '';
+		}	
+		if (isset($this->error['telephone'])) {
+			$this->data['error_telephone'] = $this->error['telephone'];
+		} else {
+			$this->data['error_telephone'] = '';
+		}	
+
+		$this->data['action'] = $this->url->link('account/edit', '', 'SSL');
+
+		if ($this->request->server['REQUEST_METHOD'] != 'POST') {
+			$customer_info = $this->model_account_customer->getCustomer($this->customer->getId());
+		}
+
+		if (isset($this->request->post['firstname'])) {
+			$this->data['firstname'] = $this->request->post['firstname'];
+		} elseif (isset($customer_info)) {
+			$this->data['firstname'] = $customer_info['firstname'];
+		} else {
+			$this->data['firstname'] = '';
+		}
+
+		if (isset($this->request->post['lastname'])) {
+			$this->data['lastname'] = $this->request->post['lastname'];
+		} elseif (isset($customer_info)) {
+			$this->data['lastname'] = $customer_info['lastname'];
+		} else {
+			$this->data['lastname'] = '';
+		}
+
+		if (isset($this->request->post['email'])) {
+			$this->data['email'] = $this->request->post['email'];
+		} elseif (isset($customer_info)) {
+			$this->data['email'] = $customer_info['email'];
+		} else {
+			$this->data['email'] = '';
+		}
+
+		if (isset($this->request->post['telephone'])) {
+			$this->data['telephone'] = $this->request->post['telephone'];
+		} elseif (isset($customer_info)) {
+			$this->data['telephone'] = $customer_info['telephone'];
+		} else {
+			$this->data['telephone'] = '';
+		}
+		if (isset($this->request->post['years_trading'])) {
+			$this->data['years_trading'] = $this->request->post['years_trading'];
+		} elseif (isset($customer_info)) {
+			$this->data['years_trading'] = $customer_info['years_trading'];
+		} else {
+			$this->data['years_trading'] = '';
+		}
+
+if (isset($this->request->post['business_type'])) {
+			$this->data['business_type'] = $this->request->post['business_type'];
+		} elseif (isset($customer_info)) {
+			$this->data['business_type'] = $customer_info['business_type'];
+		} else {
+			$this->data['business_type'] = '';
+		}
+
+if (isset($this->request->post['how_did_you_hear'])) {
+			$this->data['how_did_you_hear'] = $this->request->post['how_did_you_hear'];
+		} elseif (isset($customer_info)) {
+			$this->data['how_did_you_hear'] = $customer_info['how_did_you_hear'];
+		} else {
+			$this->data['how_did_you_hear'] = '';
+		}
+
+
+		if (isset($this->request->post['fax'])) {
+			$this->data['fax'] = $this->request->post['fax'];
+		} elseif (isset($customer_info)) {
+			$this->data['fax'] = $customer_info['fax'];
+		} else {
+			$this->data['fax'] = '';
+		}
+
+		$this->data['back'] = $this->url->link('account/account', '', 'SSL');
+
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/account/edit.tpl')) {
+			$this->template = $this->config->get('config_template') . '/template/account/edit.tpl';
+		} else {
+			$this->template = 'default/template/account/edit.tpl';
+		}
+		
+		$this->children = array(
+			'common/column_left',
+			'common/column_right',
+			'common/content_top',
+			'common/content_bottom',
+			'common/footer',
+			'common/header'	
+		);
+						
+		$this->response->setOutput($this->render());	
+	}
+
+	protected function validate() {
+		if ((utf8_strlen($this->request->post['firstname']) < 1) || (utf8_strlen($this->request->post['firstname']) > 32)) {
+			$this->error['firstname'] = $this->language->get('error_firstname');
+		}
+
+		if ((utf8_strlen($this->request->post['lastname']) < 1) || (utf8_strlen($this->request->post['lastname']) > 32)) {
+			$this->error['lastname'] = $this->language->get('error_lastname');
+		}
+
+		if ((utf8_strlen($this->request->post['email']) > 96) || !preg_match('/^[^\@]+@.*\.[a-z]{2,6}$/i', $this->request->post['email'])) {
+			$this->error['email'] = $this->language->get('error_email');
+		}
+		
+		if (($this->customer->getEmail() != $this->request->post['email']) && $this->model_account_customer->getTotalCustomersByEmail($this->request->post['email'])) {
+			$this->error['warning'] = $this->language->get('error_exists');
+		}
+
+		if ((utf8_strlen($this->request->post['telephone']) < 3) || (utf8_strlen($this->request->post['telephone']) > 32)) {
+			$this->error['telephone'] = $this->language->get('error_telephone');
+		}
+
+		// Custom field validations
+		if ($this->request->post['years_trading'] == '') {
+			$this->error['years_trading'] = $this->language->get('error_years_trading');
+		} 
+		if ($this->request->post['business_type'] == '') {
+			$this->error['business_type'] = $this->language->get('error_business_type');
+		} 
+		if ($this->request->post['how_did_you_hear'] == '') {
+			$this->error['how_did_you_hear'] = $this->language->get('error_how_did_you_hear');
+		} 
+
+		if (!$this->error) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+?>
